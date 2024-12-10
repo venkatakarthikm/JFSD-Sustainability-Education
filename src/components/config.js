bc4bf8bc8500c9b154b13a9545c53a740be69d16
vkarthikm <virat18mvk@gmail.com>
2024-12-10 20:07:00 +0530

    final

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: 'http://localhost:4444',
+  url: 'https://jfsd-backend-production-a024.up.railway.app',
     paypal: {
       clientId: 'AS9xOfscT4r1NW5Ig2ZIgXpwEsocP1Mdka1PRu1_F48H9HCPWlLMHTQNB1Ksbn6uC34BEbQJuw5-Y7pQ',  // PayPal Sandbox Client ID
       currency: 'USD',  // Default currency
